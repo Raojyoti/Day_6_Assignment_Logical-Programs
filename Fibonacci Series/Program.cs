c0cb358ba11ac084bbe8d9f96d02d802e03a42f5
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-01 13:03:08 +0530

    Fibonacci Series

---
+++ Fibonacci Series/Program.cs
@@ -0,0 +1,24 @@
+﻿/*   Fibonacci Series */
+
+using System;
+
+namespace Fibonacci_Series
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 1;
+            Console.Write("Enter number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i=0; i<=num; i++)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+       
+                Console.WriteLine(a );
+            }
+        }
+    }
+}
