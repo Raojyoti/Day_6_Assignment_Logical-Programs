d6d16711f77c06023d194c6f9a3283f4452dfdaa
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-02 20:17:50 +0530

    Swap two number

---
+++ Swap_Nibbles_Number/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Swap_Nibbles_Number
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int result = Swap.SwapNibbles(n);
+            Console.WriteLine("Original number: " + n +"  "+ "Swapped number: " + result);
+        }
+    }
+}
---
+++ Swap_Nibbles_Number/Swap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swap_Nibbles_Number
+{
+    public class Swap
+    {
+        public int n,x;
+        public static int SwapNibbles(int n)
+        {
+            int right = (n & 0b00001111);
+            right = (right << 4);
+            int left = (n & 0b11110000);
+            left = (left >> 4);
+            return (right | left);
+        }
+
+    }
+}
