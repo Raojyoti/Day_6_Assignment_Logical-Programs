a17a2fa7ebed96be1bbf82eb3e272b17ee056680
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-02 22:09:37 +0530

    Print Day of the week

---
+++ Gregorian calendar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gregorian_calendar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter day: ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter month: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Week.DayOfWeek();
+        }
+    }
+}
