dabc1d1934964bd2222024a9b897edc102860fb5
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-02 19:47:08 +0530

    Perfect number

---
+++ Perfect Number/Program.cs
@@ -0,0 +1,33 @@
+﻿/*  Perfect Number */
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Perfect_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0, result;
+            Console.Write("Enter number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            result = num;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                { 
+                    sum = sum + i;
+                }
+            }
+            if (sum == result)
+            {
+                Console.WriteLine(num + " is perfect number ");
+            }
+            else
+            { 
+                Console.WriteLine(num + " is not perfect number "); 
+            }
+        }
+        
+    }
+}
