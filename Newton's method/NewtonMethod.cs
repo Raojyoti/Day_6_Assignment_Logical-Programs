fb1d5a109d58394dad3523fdc15b18bc77af88cc
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-02 20:08:24 +0530

    Newtons Method

---
+++ Newton's method/NewtonMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Newton_s_method
+{
+    internal class NewtonMethod
+    {
+        public static double c, epsilon= epsilon = 1e-15, root, count = 0, t=c;
+        public static void Sqrt()
+        {
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (c / t + t) / 2.0;
+            }
+            Console.WriteLine(t);
+        }
+    }
+}
---
+++ Newton's method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Newton_s_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewtonMethod.Sqrt();
+        }
+    }
+}
