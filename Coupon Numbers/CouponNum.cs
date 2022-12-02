6c5021981454194784a7c0145fa519153832458a
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-02 20:22:02 +0530

    Coupon Number

---
+++ Coupon Numbers/CouponNum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coupon_Numbers
+{
+    public class CouponNum
+    {
+        public static int num=10;
+        public static string str = "ABCDEFIGHIJKLMNOPQRSTUVWXYZabcdefighjklmnopqrstuvwxyz1230456789";
+        static string randomstring = "";
+        public static void CoupNumber()
+        {
+            Random res = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                int x = res.Next(str.Length);
+                randomstring = randomstring + str[x];
+            }
+            Console.WriteLine("Coupon number is: " + randomstring);
+        }
+    }
+}
---
+++ Coupon Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Coupon_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CouponNum.CoupNumber();
+        }
+    }
+}
