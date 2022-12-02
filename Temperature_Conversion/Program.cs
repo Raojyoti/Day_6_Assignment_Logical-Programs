5305ac2819a94da9d5a846215b6ecdc27deeb57a
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-02 20:03:07 +0530

    Temperature Conversion

---
+++ Temperature_Conversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Temperature_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            {
+            Console.Write("Enter value for C: ");
+            temperatureConversion.C=Convert.ToInt32(Console.ReadLine());
+            temperatureConversion.temperaturConversionToFahrenheit();
+
+            Console.WriteLine(" \n ");
+
+            Console.Write("Enter value for F: ");
+            temperatureConversion.F = Convert.ToInt32(Console.ReadLine()); ;
+            temperatureConversion.temperaturConversionToCelsius();
+            Console.WriteLine(" \n ");
+        }
+       
+    }
+}
---
+++ Temperature_Conversion/temperatureConversion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Temperature_Conversion
+{
+    internal class temperatureConversion
+    {
+        public static float F, C, Fahrenheit, Celsius;
+        public static void temperaturConversionToFahrenheit()
+        {
+            Fahrenheit= (C * 9 / 5) +32;
+            Console.Write("Celsius to Fahrenheit: " + Fahrenheit);
+        }
+        public static void temperaturConversionToCelsius()
+        {
+            Celsius = (F - 32) * 5 / 9;
+            Console.Write("Fahrenheit to Celsius : " + Celsius);
+        }
+    }
+}
