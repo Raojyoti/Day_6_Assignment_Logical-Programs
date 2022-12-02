c081ad21c09c7afb8714b505dbbf1302ec1559b5
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-02 20:13:09 +0530

    Decimal to Binary

---
+++ Decimal_to_Binary/Binary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decimal_to_Binary
+{
+    internal class Binary
+    {
+        public static int n, result;
+        public static void DecToBinary(int n)
+        {
+            int[] binaryNum = new int[32];
+            int i = 0;
+            while (n > 0)
+            {
+                binaryNum[i] = n % 2;
+                n = n / 2;
+                i++;
+            }
+            for (int j = i - 1; j >= 0; j--)
+            { 
+                Console.Write( binaryNum[j]); 
+            }
+        }
+    }
+}
---
+++ Decimal_to_Binary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Decimal_to_Binary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Binary.DecToBinary(n);
+  
+        }
+    }
+}
