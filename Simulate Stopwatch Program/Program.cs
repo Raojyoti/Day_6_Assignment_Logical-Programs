35f0e15cd710093a5683e72f47111a93111de95b
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-02 19:59:57 +0530

    Simulate Stopwatch Program

---
+++ Simulate Stopwatch Program/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Simulate_Stopwatch_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();   
+            stopwatch.Start(); 
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(10);
+            }
+
+            stopwatch.Stop(); 
+
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+    
+}
